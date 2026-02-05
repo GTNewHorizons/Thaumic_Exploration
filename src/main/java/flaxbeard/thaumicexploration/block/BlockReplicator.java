642e9116fee6277fac3af75a56233292efcc10ee
koolkrafter5 <creepersrgreen@gmail.com>
2026-02-05 01:57:36 -0500

    Spotless

--- src/main/java/flaxbeard/thaumicexploration/block/BlockReplicator.java
+++ src/main/java/flaxbeard/thaumicexploration/block/BlockReplicator.java
@@ -7,7 +7,6 @@ import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
