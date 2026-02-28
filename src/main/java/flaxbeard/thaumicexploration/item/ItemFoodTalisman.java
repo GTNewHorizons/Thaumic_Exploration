12977cd5591f0dd9aebee2bfeeb58a4511e5a005
Andrew Hoblit <thehoblit@gmail.com>
2026-02-28 14:40:53 -0800

    Issue #23660 disabled sound, non functional

--- src/main/java/flaxbeard/thaumicexploration/item/ItemFoodTalisman.java
+++ src/main/java/flaxbeard/thaumicexploration/item/ItemFoodTalisman.java
@@ -111,10 +111,8 @@ public class ItemFoodTalisman extends Item {
                 }
                 player.inventory.decrStackSize(i, 1);
 
-                player.playSound(
-                        "random.eat",
-                        0.5F + 0.5F * (float) player.worldObj.rand.nextInt(2),
-                        (player.worldObj.rand.nextFloat() - player.worldObj.rand.nextFloat()) * 0.2F + 1.0F);
+                //TODO Fix sound not playing
+                //player.playSound("random.eat", 1f, 1f);
 
             }
         }
