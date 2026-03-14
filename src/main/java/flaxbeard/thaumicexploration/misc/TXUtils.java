ddf9b943d0bcd81f849e694e25ac6aeac88f60d2
thehoblit <thehoblit@gmail.com>
2026-03-14 14:48:33 -0700

    Fix offline warp for real this time
    I didn't know thaum uses compressed nbt

--- src/main/java/flaxbeard/thaumicexploration/misc/TXUtils.java
+++ src/main/java/flaxbeard/thaumicexploration/misc/TXUtils.java
@@ -1,5 +1,7 @@
 package flaxbeard.thaumicexploration.misc;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -23,8 +25,7 @@ public class TXUtils {
 
     public static void addWarpPermOfflinePlayer(String owner, int addedWarp) {
 
-        Path playerDataDir = DimensionManager.getCurrentSaveRootDirectory().toPath()
-            .resolve("playerdata");
+        Path playerDataDir = DimensionManager.getCurrentSaveRootDirectory().toPath().resolve("playerdata");
         Path playerDataPath = playerDataDir.resolve(owner + ".thaum");
 
         if (!Files.exists(playerDataPath)) {
@@ -32,11 +33,11 @@ public class TXUtils {
         }
 
         NBTTagCompound playerData;
-        try {
-            playerData = CompressedStreamTools.read(playerDataPath.toFile());
+        try (FileInputStream fis = new FileInputStream(playerDataPath.toFile())) {
+            playerData = CompressedStreamTools.readCompressed(fis);
         } catch (IOException e) {
             e.printStackTrace();
-            Thaumcraft.log.fatal("Error reading {}'s Thaumcraft data", owner);
+            Thaumcraft.log.fatal("Error reading {}'s Thaumcraft data at {}.", owner, playerDataPath.toString());
             return;
         }
 
@@ -44,20 +45,21 @@ public class TXUtils {
         playerData.setInteger("Thaumcraft.eldritch", currentWarp + addedWarp);
 
         Path tempPath = playerDataDir.resolve(owner + "_temp.thaum");
-        try {
-            CompressedStreamTools.write(playerData, tempPath.toFile());
+        try (FileOutputStream fos = new FileOutputStream(tempPath.toFile())) {
+            CompressedStreamTools.writeCompressed(playerData, fos);
             Files.move(tempPath, playerDataPath, StandardCopyOption.REPLACE_EXISTING);
-        }
-        catch (IOException | SecurityException e) {
+        } catch (IOException | SecurityException e) {
             e.printStackTrace();
-            Thaumcraft.log.fatal("Error writing {}'s Thaumcraft data", owner);
+            Thaumcraft.log.fatal("Error writing {}'s Thaumcraft data at {}", owner, tempPath.toString());
         }
 
         try {
             Files.deleteIfExists(tempPath);
         } catch (IOException e) {
             e.printStackTrace();
-            Thaumcraft.log.fatal("Error deleting {}'s temporary Thaumcraft data", owner);
+            Thaumcraft.log.fatal("Error deleting {}'s temporary Thaumcraft data at {}", owner, tempPath.toString());
         }
+
+        Thaumcraft.log.info("Returned {} warp to {} from their Soul Brazier", addedWarp, owner);
     }
 }
